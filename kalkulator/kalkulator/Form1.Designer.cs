e2229fae007db21947fd9923e95768995aa5be19
Wiktor Matusiak <115088746+wiktor2002@users.noreply.github.com>
2023-11-13 13:54:26 +0100

    Add files via upload

---
+++ kalkulator/kalkulator/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace kalkulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbWynik = new System.Windows.Forms.TextBox();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.bDodawanie = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.bOdejmowanie = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.bMnozenie = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.bWynik = new System.Windows.Forms.Button();
+            this.bDzielenie = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbWynik
+            // 
+            this.tbWynik.Location = new System.Drawing.Point(64, 24);
+            this.tbWynik.Name = "tbWynik";
+            this.tbWynik.Size = new System.Drawing.Size(146, 20);
+            this.tbWynik.TabIndex = 0;
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(12, 50);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(50, 38);
+            this.b7.TabIndex = 1;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(84, 50);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(49, 38);
+            this.b8.TabIndex = 2;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(150, 50);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(44, 38);
+            this.b9.TabIndex = 3;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // bDodawanie
+            // 
+            this.bDodawanie.Location = new System.Drawing.Point(218, 49);
+            this.bDodawanie.Name = "bDodawanie";
+            this.bDodawanie.Size = new System.Drawing.Size(43, 40);
+            this.bDodawanie.TabIndex = 4;
+            this.bDodawanie.Text = "+";
+            this.bDodawanie.UseVisualStyleBackColor = true;
+            this.bDodawanie.Click += new System.EventHandler(this.bDodawanie_Click);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(13, 118);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(49, 43);
+            this.b4.TabIndex = 5;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(84, 118);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(49, 43);
+            this.b5.TabIndex = 6;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(150, 118);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(44, 43);
+            this.b6.TabIndex = 7;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // bOdejmowanie
+            // 
+            this.bOdejmowanie.Location = new System.Drawing.Point(218, 118);
+            this.bOdejmowanie.Name = "bOdejmowanie";
+            this.bOdejmowanie.Size = new System.Drawing.Size(43, 43);
+            this.bOdejmowanie.TabIndex = 8;
+            this.bOdejmowanie.Text = "-";
+            this.bOdejmowanie.UseVisualStyleBackColor = true;
+            this.bOdejmowanie.Click += new System.EventHandler(this.bOdejmowanie_Click);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(14, 184);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(48, 39);
+            this.b1.TabIndex = 9;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(84, 184);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(49, 39);
+            this.b2.TabIndex = 10;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(150, 184);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(44, 39);
+            this.b3.TabIndex = 11;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // bMnozenie
+            // 
+            this.bMnozenie.Location = new System.Drawing.Point(218, 184);
+            this.bMnozenie.Name = "bMnozenie";
+            this.bMnozenie.Size = new System.Drawing.Size(43, 39);
+            this.bMnozenie.TabIndex = 12;
+            this.bMnozenie.Text = "*";
+            this.bMnozenie.UseVisualStyleBackColor = true;
+            this.bMnozenie.Click += new System.EventHandler(this.bMnozenie_Click);
+            // 
+            // b0
+            // 
+            this.b0.Location = new System.Drawing.Point(14, 242);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(48, 42);
+            this.b0.TabIndex = 13;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.Click += new System.EventHandler(this.b0_Click);
+            // 
+            // bWynik
+            // 
+            this.bWynik.Location = new System.Drawing.Point(91, 247);
+            this.bWynik.Name = "bWynik";
+            this.bWynik.Size = new System.Drawing.Size(103, 37);
+            this.bWynik.TabIndex = 14;
+            this.bWynik.Text = "=";
+            this.bWynik.UseVisualStyleBackColor = true;
+            this.bWynik.Click += new System.EventHandler(this.bWynik_Click);
+            // 
+            // bDzielenie
+            // 
+            this.bDzielenie.Location = new System.Drawing.Point(218, 242);
+            this.bDzielenie.Name = "bDzielenie";
+            this.bDzielenie.Size = new System.Drawing.Size(43, 42);
+            this.bDzielenie.TabIndex = 15;
+            this.bDzielenie.Text = "/";
+            this.bDzielenie.UseVisualStyleBackColor = true;
+            this.bDzielenie.Click += new System.EventHandler(this.bDzielenie_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 302);
+            this.Controls.Add(this.bDzielenie);
+            this.Controls.Add(this.bWynik);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.bMnozenie);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.bOdejmowanie);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.bDodawanie);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.tbWynik);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "kalkulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbWynik;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button bDodawanie;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button bOdejmowanie;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button bMnozenie;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button bWynik;
+        private System.Windows.Forms.Button bDzielenie;
+    }
+}
+
